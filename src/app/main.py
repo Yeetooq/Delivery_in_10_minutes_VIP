6b01392762b4257249239abccc0c2c8af8d64777
Yeetooq <roma20040402@gmail.com>
2025-01-24 07:00:27 +0300

    The project structure has been redesigned and added .exe file

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,15 @@
 import os
 import webview
 
+# Определяем путь к директории, где находится сам скрипт
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 def load_addresses():
     addresses = []
     try:
         # Путь к файлу addresses.txt
-        addresses_path = os.path.join('src', 'resources', 'addresses.txt')
+        addresses_path = os.path.join(BASE_DIR, 'addresses.txt')
         with open(addresses_path, 'r', encoding='utf-8') as file:
             for line in file:
                 addresses.append(line.strip())
@@ -13,11 +17,14 @@ def load_addresses():
         print(f"Ошибка при загрузке адресов из файла: {e}")
     return addresses
 
+
 if __name__ == "__main__":
     # Загрузка адресов
     addresses = load_addresses()
 
+    # Путь к файлу map.html
+    map_path = os.path.join(BASE_DIR, 'map.html')
+
     # Инициализация WebView
-    map_path = os.path.abspath(os.path.join('src', 'resources', 'map.html'))
     webview.create_window('Доставка за 10 минут', f'file:///{map_path}', width=800, height=600)
     webview.start()
